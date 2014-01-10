69c47523847cf8763bed74dd5e675ee7363c93e5
Randy Hicks <randy@visualcoma.com>
2014-01-10 15:04:53 -0600

    added tpl-test.php - this file allows you to do php testing for various purposes.

---
+++ tpl-testing.php
@@ -0,0 +1,16 @@
+<?php
+/* Template Name: Testing */
+
+/**
+ * File Name tpl-testing.php
+ * @package WordPress
+ * @subpackage ParentTheme
+ * @license GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @version 1.0
+ * @updated 00.00.00
+ **/
+#################################################################################################### */
+
+
+
+print_r($wp_query);
