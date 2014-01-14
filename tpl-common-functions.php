40dd91a1e89bd29480bccae742a77b8c7aeaf61d
Randy Hicks <randy@visualcoma.com>
2014-01-13 18:09:30 -0600

    update 1.1.2 - update common functions template

--- tpl-common-functions.php
+++ tpl-common-functions.php
@@ -1,5 +1,5 @@
 <?php
-/* Template Name: Common Functions Example */
+/* Template Name: Common Functionality */
 
 /**
  * File Name tpl-common-functions.php
@@ -14,134 +14,39 @@
  **/
 #################################################################################################### */
 
-?><!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8) ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
-</head>
+get_header(); ?>
 
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<?php if ( get_header_image() ) : ?>
-	<div id="site-header">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-		</a>
-	</div>
-	<?php endif; ?>
+<div id="main-content" class="main-content">
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-
-			<div class="search-toggle">
-				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
-			</div>
-
-			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-				<h1 class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></h1>
-				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-			</nav>
-		</div>
-
-		<div id="search-container" class="search-box-wrapper hide">
-			<div class="search-box">
-				<?php get_search_form(); ?>
-			</div>
-		</div>
-	</header><!-- #masthead -->
-
-	<div id="main" class="site-main">
-
-		<div id="main-content" class="main-content">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
 
 		<?php
-			if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-				// Include the featured content template.
-				get_template_part( 'featured-content' );
-			}
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post();
+					?>
+					
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					
+						<?php the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header>' ); ?>
+					
+						<div class="entry-content">
+							<?php the_content(); ?>
+						</div><!-- .entry-content -->
+						
+					</article><!-- #post-## -->
+					
+					<?php
+					
+				} // end while ( have_posts() )
+			} // end if ( have_posts() )
 		?>
 
-			<div id="primary" class="content-area">
-				<div id="content" class="site-content" role="main">
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	
+</div><!-- #main-content -->
 
-				<?php
-					if ( have_posts() ) :
-						// Start the Loop.
-						while ( have_posts() ) : the_post();
-							
-							?>
-							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-								<?php
-									// Page thumbnail and title.
-									twentyfourteen_post_thumbnail();
-									the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' );
-								?>
-
-								<div class="entry-content">
-									<?php
-										the_content();
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-
-										edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
-									?>
-								</div><!-- .entry-content -->
-							</article><!-- #post-## -->
-							<?php
-
-						endwhile;
-						// Previous/next post navigation.
-						twentyfourteen_paging_nav();
-
-					else :
-						// If no content, include the "No posts found" template.
-						get_template_part( 'content', 'none' );
-
-					endif;
-				?>
-
-				</div><!-- #content -->
-			</div><!-- #primary -->
-			<?php get_sidebar( 'content' ); ?>
-		</div><!-- #main-content -->
-		
-		<?php get_sidebar(); ?>
-		
-	   	</div><!-- #main -->
-
-		<footer id="colophon" class="site-footer" role="contentinfo">
-
-			<?php get_sidebar( 'footer' ); ?>
-
-			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
-
-	<?php wp_footer(); ?>
-</body>
-</html>
+<?php
+get_footer();
